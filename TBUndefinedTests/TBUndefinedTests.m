12563cfadb4fee58d6e5c8503f51f4c7e1f36c26
Markos Charatzas <cuenoid@gmail.com>
2013-02-23 21:31:56 +0000

    Added symbol tables and class dumps

--- TBUndefinedTests/TBUndefinedTests.m
+++ TBUndefinedTests/TBUndefinedTests.m
@@ -6,23 +6,13 @@
 //  Copyright (c) 2013 Markos Charatzas (@qnoid). All rights reserved.
 //
 
-#import "TBUndefinedTests.h"
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TBUndefinedTests : SenTestCase
+
+@end
 
 @implementation TBUndefinedTests
-
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
 
 - (void)testExample
 {
